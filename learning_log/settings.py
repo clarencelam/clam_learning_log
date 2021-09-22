b52df0892e5f63432560330defd2e7f286700a25
clarencelam <lam.clarence.bc@gmail.com>
2021-09-21 19:20:30 -0700

    rdy4

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -142,6 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'users:login'
 
 # Heroku settings:
-import os
 import django_heroku
 django_heroku.settings(locals())
